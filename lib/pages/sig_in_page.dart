108a71adfc511c7e5e0af7d4c593f818ae471ef7
ROG <dyavas21@gmail.com>
2022-01-24 21:17:45 +0700

    fluttertoast

--- lib/pages/sig_in_page.dart
+++ lib/pages/sig_in_page.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:furniture_app/theme.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  late FToast fToast;
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+
+  bool isShowPasswordError = false;
+  bool isRememberMe = false;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,6 +118,7 @@ class SignInPage extends StatelessWidget {
         ),
       ),
       child: TextFormField(
+        controller: emailController,
         decoration: InputDecoration.collapsed(
           hintText: 'Email',
           hintStyle: greyTextStyle.copyWith(
@@ -109,39 +131,53 @@ class SignInPage extends StatelessWidget {
   }
 
   Widget passwordInput() {
-    return Container(
-      margin: EdgeInsets.only(
-        top: 32,
-      ),
-      padding: EdgeInsets.all(
-        16,
-      ),
-      decoration: BoxDecoration(
-        color: kWhiteGreyColor,
-        borderRadius: BorderRadius.circular(
-          14,
-        ),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration.collapsed(
-                hintText: 'Password',
-                hintStyle: greyTextStyle.copyWith(
-                  fontSize: 16,
-                  fontWeight: semibold,
-                ),
-              ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 32,
+          ),
+          padding: EdgeInsets.all(
+            16,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteGreyColor,
+            borderRadius: BorderRadius.circular(
+              14,
             ),
           ),
-          Icon(
-            Icons.visibility_outlined,
-            color: kGreyColor,
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Password',
+                    hintStyle: greyTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semibold,
+                    ),
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.visibility_outlined,
+                color: kGreyColor,
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        if (isShowPasswordError)
+          Container(
+            margin: EdgeInsets.only(top: 8),
+            child: Text(
+              'Password kamu salah',
+              style: redTextStyle,
+            ),
+          ),
+      ],
     );
   }
 
@@ -156,8 +192,12 @@ class SignInPage extends StatelessWidget {
             width: 20,
             height: 20,
             child: Checkbox(
-              value: false,
-              onChanged: (value) {},
+              value: isRememberMe,
+              onChanged: (value) {
+                setState(() {
+                  isRememberMe = value!;
+                });
+              },
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(4),
               ),
@@ -182,20 +222,45 @@ class SignInPage extends StatelessWidget {
         top: 32,
       ),
       child: TextButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            isLoading = true;
+          });
+
+          Future.delayed(Duration(seconds: 2), () {
+            setState(() {
+              isLoading = false;
+            });
+            if (passwordController.text != '123123') {
+              setState(() {
+                isShowPasswordError = true;
+              });
+              fToast.showToast(
+                child: errorToast(),
+                toastDuration: Duration(seconds: 2),
+                gravity: ToastGravity.BOTTOM,
+              );
+            }
+          });
+        },
         style: TextButton.styleFrom(
           backgroundColor: kBlackColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(14),
           ),
         ),
-        child: Text(
-          'Login',
-          style: whiteTextStyle.copyWith(
-            fontSize: 18,
-            fontWeight: semibold,
-          ),
-        ),
+        child: isLoading
+            ? CircularProgressIndicator(
+                color: kWhiteColor,
+                backgroundColor: kGreyColor,
+              )
+            : Text(
+                'Login',
+                style: whiteTextStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: semibold,
+                ),
+              ),
       ),
     );
   }
@@ -225,7 +290,7 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  registerButton() {
+  Widget registerButton() {
     return Container(
       margin: EdgeInsets.only(
         top: 48,
@@ -251,6 +316,23 @@ class SignInPage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget errorToast() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: kRedColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        'Password salah',
+        style: whiteTextStyle.copyWith(
+          fontSize: 16,
+          fontWeight: semibold,
+        ),
       ),
     );
   }
